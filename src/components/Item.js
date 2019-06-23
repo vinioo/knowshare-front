f0c139c9366ec80024647f688b1d503a50e57e64
joaobenthin <joaopaulobenthin@hotmail.com>
2019-06-23 17:00:04 -0300

    heroku env

--- src/components/Item.js
+++ src/components/Item.js
@@ -16,7 +16,7 @@ export default class Item extends Component {
   async componentDidMount() {
     this.registerToSocket();
 
-    await axios.get(`${process.env.URL}posts`).then(res => {
+    await axios.get(`https://knowshare-back.herokuapp.com/posts`).then(res => {
       const items = res.data;
       this.setState({ items: items });
     });
@@ -39,11 +39,13 @@ export default class Item extends Component {
       });
     });
   };
-  handleLike = id => {
-    axios.post(`${process.env.URL}/posts/${id}/like`);
+  handleLike = async id => {
+    await axios.post(`https://knowshare-back.herokuapp.com/posts/${id}/like`);
   };
-  handleDislike = id => {
-    axios.post(`${process.env.URL}/posts/${id}/dislike`);
+  handleDislike = async id => {
+    await axios.post(
+      `https://knowshare-back.herokuapp.com/posts/${id}/dislike`
+    );
   };
   getVideoThumbnail = link => {
     if (link !== undefined && link !== null) {
