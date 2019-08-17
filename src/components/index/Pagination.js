50d2026876a299ff190ca5ba773cef12da11c1bc
Vinicius <vi.ni.cius4@hotmail.com>
2019-08-17 18:23:54 -0300

    Paginacao e ajustes finos

--- src/components/index/Pagination.js
+++ src/components/index/Pagination.js
@@ -4,8 +4,17 @@ import './Pagination.css'
 export default class Pagination extends Component {
     render() {
         return (
-            <div className="Pagination">
-                
+            <div className="PaginationContainer">
+                <div class="pagination">
+                    <p>&laquo;</p>
+                    <p class="active">1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                    <p>5</p>
+                    <p>6</p>
+                    <p>&raquo;</p>
+                </div>
             </div>
         )
     }
