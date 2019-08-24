f0e51c153185b940bf9f521e71199f7c95e4d854
joaobenthin <joaopaulobenthin@hotmail.com>
2019-08-24 14:32:44 -0300

    chore: eslint, prettier, editorconfig

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import GlobalStyle from "./styles/global";
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Content from "./pages/Content";
-import Login from "./pages/Login";
-import NewPost from "./pages/NewPost";
-import "./App.css";
+import Homepage from './pages/Homepage';
+import Content from './pages/Content';
+import Login from './pages/Login';
+import NewPost from './pages/NewPost';
+import './App.css';
 
 function App() {
   return (
